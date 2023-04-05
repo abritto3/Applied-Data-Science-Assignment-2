a3844f8a8184bb28359f2f3beaf280c8c41e111a
abritto3 <54309059+abritto3@users.noreply.github.com>
2023-04-05 21:11:29 +0100

    Update Assignment 2.py

--- Assignment 2.py
+++ Assignment 2.py
@@ -1,46 +1,68 @@
-   
+'''
+Importing the modules 
+stats module is imported to calculate the Skew and Kurtosis values of a data set
+'''   
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import gmean, variation
 from scipy.stats import stats
-from matplotlib import cm
 import seaborn as sns
 
 def read_file(filename, Countries, Years):
     """The function read_file is defined to read the excel file and will
     return 2 dataframes, one with countries as coloumns and one with years as
     coloumns"""
+    #Read the data set and delete the first 3 rows of the excel file containing irrelevent data
     x = pd.read_excel(filename, skiprows=3)
     x.drop(columns = ['Country Code', 'Indicator Name', 'Indicator Code'], axis=1, inplace=True)
+    #index the contry name column
     x.set_index(['Country Name'], inplace=True)
+    #Call the data set containg only the relevent Countries and Years
     y = x.iloc[Countries, Years]
+    #Retrieve the transpose of the data set
     z = y.T
-
+    #Return the data set and the transposed data set
     return y, z
 
-def heat_map(filename, Country, heat_i, heat_y, cma):
+def heat_map(filename, Country, heat_i, cma):
+    """The function heat_map is used to plot a heat map taking in the excel file containing
+    the inital data set, the particular country, indicators, years and the heat map 
+    color scheme as it's arguments'
+    """
     a = pd.read_excel(r"C:\Users\HP\OneDrive - University of Hertfordshire\Assignments\Assignment 2\Climate Change.xls", skiprows = 3)
+    #Drop the irrelevent columns from the data set 
     a.drop(columns = ['Country Code' , 'Indicator Code'], axis=1, inplace=True)
+    #Select the columns containing the specified country
     b = a[a.iloc[:,0] == str(Country)]
     b.drop('Country Name' , axis=1, inplace=True)
     b.set_index('Indicator Name', inplace=True)
+    #Transpose the data set with year as rows and indicators as columns
     b = b.T
+    #retrieve the data set with only the required indicators
     c = b.loc[:, heat_i]
     plt.figure(figsize = (10, 5))
+    #Plot the heat map
     sns.heatmap(c.corr(), vmin=-1, vmax=1, annot=True, cmap = str(cma), fmt=".3f")
+    #Set the title of the map as country name
     plt.title(str(Country))
     return
 
-#Countries = [35, 40, 55, 81, 109, 119, 202, 205, 251]
+#Array containing the index of the required countries
 Countries = [35, 40, 55, 81, 109, 119, 70, 205, 251]
+#Array containing thebindex of required years 1995 -2010
 Years = [35, 40, 45, 50]
 
+'''Retrieve the data sets into the first set and the transpose of the same in the second set
+   Pass the file name and the array containing indexes of Countries and Years as the arguments
+   to the function
+'''
 co2_1, co2_2 = read_file(r"C:\Users\HP\OneDrive - University of Hertfordshire\Assignments\Assignment 2\CO2 emissions (kt).xls", Countries, Years)
 electric_1, electric_2 = read_file(r"C:\Users\HP\OneDrive - University of Hertfordshire\Assignments\Assignment 2\Electric power consumption (kWh per capita).xls", Countries,Years) 
 ren_1, ren_2 = read_file(r"C:\Users\HP\OneDrive - University of Hertfordshire\Assignments\Assignment 2\Renewable electricity output (% of total electricity output).xls", Countries,Years) 
 energy_1, energy_2 = read_file(r"C:\Users\HP\OneDrive - University of Hertfordshire\Assignments\Assignment 2\Energy use (kg of oil equivalent per capita).xls", Countries,Years) 
 
+'''Plot the graphs with the data sets retrieved and set the title and and labels
+   Rotate the x ticks so that the country names do not intertwine in the graph
+'''
 co2_1.plot(kind="bar", alpha = 0.8)
 plt.title("CO2 emissions rate")
 plt.xlabel("Countries")
@@ -51,6 +73,7 @@ electric_1.plot(kind="bar")
 plt.title("Electric power consumption rate")
 plt.xlabel("Countries")
 plt.ylabel("Electric power consumption (kWh per capita)")
+plt.legend(loc='upper right', bbox_to_anchor=(1.3, 1), borderaxespad=0)
 plt.xticks(rotation = 45)
 
 ren_1.plot(kind="line")
@@ -67,11 +90,12 @@ plt.xticks(rotation = 45)
 
 plt.show()
 
+#Array containing the indicator names specifying the indicators needed to plot on the heat maps
 heat_indicators = ["Annual freshwater withdrawals, total (billion cubic meters)", "Population growth (annual %)", "Methane emissions (kt of CO2 equivalent)", "Electricity production from oil sources (% of total)", "Electric power consumption (kWh per capita)"]
-heat_years = ['2000','2005','2010','2015','2020']
+#Plot two heat maps with different country names  and the indicators and map collor scheme as arguments
+heat_map(r"C:\Users\HP\OneDrive - University of Hertfordshire\Assignments\Assignment 2\Climate Change.xls", "United States", heat_indicators, 'Pastel1')
+heat_map(r"C:\Users\HP\OneDrive - University of Hertfordshire\Assignments\Assignment 2\Climate Change.xls", "Spain", heat_indicators, 'Set3')
 
-heat_map(r"C:\Users\HP\OneDrive - University of Hertfordshire\Assignments\Assignment 2\Climate Change.xls", "China", heat_indicators, heat_years, 'prism')
-heat_map(r"C:\Users\HP\OneDrive - University of Hertfordshire\Assignments\Assignment 2\Climate Change.xls", "India", heat_indicators, heat_years, 'prism')
-
+#Print the Skew and Kurtosis of a used data set
 print("Skew:", stats.skew(ren_1["1995"]))
 print("Kurtosis", stats.kurtosis(ren_1["1995"]))
